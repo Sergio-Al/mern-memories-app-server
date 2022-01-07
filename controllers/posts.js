a55100dd3466cfd4d6ec652720d4e4ddea9a5c09
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2022-01-07 02:07:34 -0400

    Pagination & Search
    We added new features to this project
    - search by coincidence
    - smart pagination
    - All is worked as expected

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,11 +2,51 @@ import mongoose from "mongoose";
 import PostMessage from "../models/postMessages.js";
 
 export const getPosts = async (req, res) => {
+  const { page } = req.query;
   try {
-    const postMesasges = await PostMessage.find();
+    const LIMIT = 8; // Number of post per page
+    const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
+    const total = await PostMessage.countDocuments({});
+
+    const posts = await PostMessage.find()
+      .sort({ _id: -1 })
+      .limit(LIMIT)
+      .skip(startIndex < 0 ? 0 : startIndex);
     //console.log(postMesasges);
 
-    res.status(200).json(postMesasges);
+    res.status(200).json({
+      data: posts,
+      currentPage: Number(page),
+      numberOfPages: Math.ceil(total / LIMIT),
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getPost = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const post = await PostMessage.findById(id);
+
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+// QUERY -> /posts?page=2 -> page = 2
+// PARAMS -> /posts/123 -> id = 123 // because we establish /posts/:id
+export const getPostsBySearch = async (req, res) => {
+  const { searchQuery, tags } = req.query;
+  try {
+    const title = new RegExp(searchQuery, "i");
+    const posts = await PostMessage.find({
+      $or: [{ title }, { tags: { $in: tags.split(",") } }], // REMEMBER $or and $id read array params
+    });
+
+    res.json({ data: posts });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
